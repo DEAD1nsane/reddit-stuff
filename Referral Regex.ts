ae9e7f2d9fec37ecd3fa1372f433ee1fc11a9e1f
DEAD1nsane <turbolaceup@gmail.com>
2025-02-08 11:20:25 -0600

    update

--- Referral Regex.ts
+++
@@ -1,9 +0,0 @@
-# regex to catch a lot of different types of referral links for automation preventing users from posting or commenting if a referral is matched.
-
-(?:ref(?:=|\/)|referral|invite|code|coupon|discount|promo|partner|affiliate|unique|track|campaign|source|id|uid|user|customer|member|friend|bonus|reward|earn|mid|referralCode|signup(?:\/|=)|ref_code(?:\/|=)|\/?referred_by_id)\/earn=|\?c=
-
-
-
-# image of how the automation should look and once you got one made you can duplicate it, rename it slightly (i just capitalized a letter) and change the type so youll have this for post and comments.
-
-https://raw.githubusercontent.com/DEAD1nsane/TurboThemes/refs/heads/main/Unknown/Automation.jpg
